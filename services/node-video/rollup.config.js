b149c29831b0b774a06528f5b5849eaafaaf59a6
jens <jens@doellmann.com>
2021-09-16 23:14:01 +0200

    update packages

--- services/node-video/rollup.config.js
+++ services/node-video/rollup.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
 
 export default args => {
   const plugins = [typescript()]
